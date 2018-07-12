fc3088f7acd9689b7c9dc902ffd1052fea628561
Lyndsey Padget <lpadget@longplaysoftware.com>
2018-07-12 13:00:02 -0500

    Add validation to zip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 import { OnInit } from '@angular/core';
 import { Customer } from './customer';
 import { FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Address } from './address';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -21,23 +22,23 @@ export class AppComponent implements OnInit {
 
   public ngOnInit(): void {
     this.customer = new Customer();
-    this.address = new Address();
-    this.createForm(this.customer, this.address);
+    this.customer.address = new Address();
+    this.createForm(this.customer);
   }
 
-  private createForm(customer: Customer, address: Address) {
+  private createForm(customer: Customer) {
     this.orderForm = this.fb.group({
       customer: this.fb.group({
         firstName: customer.firstName,
         lastName: customer.lastName,
-        age: customer.age
-      }),
-      address: this.fb.group({
-        address1: address.address1,
-        address2: address.address2,
-        city: address.city,
-        state: address.address1,
-        postalCode: address.postalCode,
+        age: customer.age,
+        address: this.fb.group({
+          address1: customer.address.address1,
+          address2: customer.address.address2,
+          city: customer.address.city,
+          state: customer.address.address1,
+          postalCode: [customer.address.postalCode, [Validators.required, Validators.pattern(/^\d{5}(?:[-\s]\d{4})?$/)]],
+        }),
       }),
     });
   }
--- src/app/customer.ts
+++ src/app/customer.ts
@@ -1,5 +1,8 @@
+import { Address } from "./address";
+
 export class Customer {
   public firstName: String = 'Jane';
   public lastName: String = 'Doe';
   public age: number = 18;
+  public address: Address = new Address();
 }
